879afa52347177ecf50e03329ccc57ff6b15e1b5
Coslate <coslate@media.ee.ntu.edu.tw>
2020-03-09 19:21:31 +0800

    Update app,js for mysql connection and query.

--- Final_Project/app.js
+++ Final_Project/app.js
@@ -20,7 +20,22 @@ GenerateAddress();
 var connection = mysql.createConnection({
     host:'localhost',
     user:'root',
+    password:process.env.MYSQL_PW,
     database:'join_us'
 }); 
+
+//var q = "SELECT 1+1 AS solution";
+//var q = "SELECT 1+5";
+var q = "SELECT CURTIME() as cur_time, CURDATE() as cur_date, NOW() as now_time";
+connection.query(q, function(error, results, fields){
+  if(error) throw error;
+  console.log("The time: ", results[0].cur_time);
+  console.log("The date: ", results[0].cur_date);
+  console.log("Now is  : ", results[0].now_time);
+})
+
+connection.end();
+
+
 
 
