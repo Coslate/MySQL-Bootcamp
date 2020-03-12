75718b03680bb7ea4b673ab5ee27428051dfccc6
Coslate <coslate@media.ee.ntu.edu.tw>
2020-03-13 00:25:56 +0800

    Remove MyJoinUs.

--- Final_Project/MyJoinUs/schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE users(
-    email VARCHAR(255) PRIMARY KEY,
-    created_at TIMESTAMP NOT NULL DEFAULT NOW()
-);
-
-INSERT INTO users(email) VALUES
-('katie34@yahoo.com'),
-('Tunde@gmail.com');
